3e0b75cd45b2cdc0244a38e36c6d9694ed704dee
watayuz <w-desu@i.softbank.jp>
2016-10-25 00:26:57 +0900

    login.js add process

--- test/login.js
+++ test/login.js
@@ -1,6 +1,31 @@
+'use strict';
+
 var page = require('webpage').create();
+var url = "https://qiita.com";
+
+page.viewportSize = {
+    width: 1024,
+    height: 768
+};
+
+// ID
+// id="identity"
+// PASSWORD
+// id="password"
 
 // settings
 phantom.cookiesEnabled = true;
 phantom.javascriptEnabled = true;
 
+page.open(url, function (status) {
+    if (status === "success") {
+
+        var id = document.getElementById('idenitty');
+        var pw = document.getElementById('password');
+
+        // do
+
+        phantom.exit();
+    }
+
+});
